6fc845c55ec42abf3ddf70c1bfe33259b1ea9d86
simrangujrati <simran.gujrati@datagrokr.com>
2020-07-04 02:54:22 +0530

    modified tests

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,15 +1,20 @@
+import pandas as pd
+
+data = [['Ram',15000], ['Raj',25000], ['Rajni',5000],['Rahul',20000]]
+
+df= pd.DataFrame(data,columns=['Name','Salary'])
 class DbHelper:
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
         '''
-        return None
+        return df['Salary'].max()
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+        return df['Salary'].max()
 
 if __name__ == "__main__":
     db_helper = DbHelper()
